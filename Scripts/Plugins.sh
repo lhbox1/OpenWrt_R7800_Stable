fac515b388aeb410db9c7bf1bc51f75b0f46cfb3
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-03-09 22:35:12 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -16,7 +16,7 @@ git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/
 #Pass Wall
 #git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall.git ./pw_luci
 #git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall-packages.git ./pw_packages
-git clone --depth=1 --single-branch --branch "master" https://github.com/kenzok8/openwrt-packages.git
+#git clone --depth=1 --single-branch --branch "master" https://github.com/kenzok8/openwrt-packages.git
 
 git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall.git
 git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall2.git
