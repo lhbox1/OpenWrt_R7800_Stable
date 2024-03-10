3e76123fab70d76476c0ef847bf5ba14ea6bd195
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-03-10 08:02:05 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -24,6 +24,9 @@ git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji
 #git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall2.git
 #git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall-packages.git
 
+#alit列表
+git clone --depth=1 --single-branch --branch "master" https://github.com/sbwml/luci-app-alist.git
+
 #预置OpenClash内核和GEO数据
 export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
 
