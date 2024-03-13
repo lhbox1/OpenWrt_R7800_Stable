fcda47d4d94aaec61ad146e77f3d63e37809da1c
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-03-14 06:12:38 +0800

    Update diy-pa.sh

--- Scripts/diy-pa.sh
+++ Scripts/diy-pa.sh
@@ -8,11 +8,11 @@
 
 #kenzok8下载使用master版本packages
 wget  -qO- https://github.com/xiaorouji/openwrt-passwall/archive/main.zip > temp1.zip
-wget  -qO- https://github.com/kenzok8/small/archive/master.zip > temp2.zip
+#wget  -qO- https://github.com/kenzok8/small/archive/master.zip > temp2.zip
 #wget  -qO- https://github.com/kenzok8/openwrt-packages/archive/master.zip > temp3.zip
 
 unzip -q temp1.zip
-unzip -q temp2.zip
+#unzip -q temp2.zip
 #unzip -q temp3.zip
 cp -r openwrt-passwall-main/luci-app-passwall package/
 
@@ -26,14 +26,14 @@ cp -r openwrt-passwall-main/luci-app-passwall package/
 #cp -r openwrt-packages-master/smartdns package/
 
 
-cp -r small-master/luci-app-ssr-plus package/
+#cp -r small-master/luci-app-ssr-plus package/
 
 #cp -r small-master/luci-app-passwall2 package/
 #cp -r small-master/dns2socks package/
 #cp -r small-master/dns2tcp package/
 
-cp -r small-master/xray-core package/
-cp -r small-master/chinadns-ng package/
+#cp -r small-master/xray-core package/
+#cp -r small-master/chinadns-ng package/
 #cp -r small-master/sing-box package/
 #cp -r small-master/luci-app-mosdns package/
 #cp -r small-master/v2dat package/
@@ -44,8 +44,8 @@ cp -r small-master/chinadns-ng package/
 rm -rf openwrt-passwall-main
 rm -rf temp1.zip
 
-rm -rf temp2.zip
-rm -rf small-master
+#rm -rf temp2.zip
+#rm -rf small-master
 
 #rm -rf temp3.zip
 #rm -rf openwrt-packages-master
