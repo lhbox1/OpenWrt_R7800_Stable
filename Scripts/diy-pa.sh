0cc21657b40afd59676a69c640f346f4eac998e5
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-03-20 23:05:30 +0800

    Update diy-pa.sh

--- Scripts/diy-pa.sh
+++ Scripts/diy-pa.sh
@@ -1,22 +1,22 @@
 #
 #下载使用master版本openclash
-#wget  -qO- https://github.com/vernesong/OpenClash/archive/master.zip > temp.zip
-#unzip -q temp.zip
-#cp -r OpenClash-master/luci-app-openclash package/
-#rm -rf OpenClash-master
-#rm -rf temp.zip
+wget  -qO- https://github.com/vernesong/OpenClash/archive/master.zip > temp.zip
+unzip -q temp.zip
+cp -r OpenClash-master/luci-app-openclash package/
+rm -rf OpenClash-master
+rm -rf temp.zip
 
 #kenzok8下载使用master版本packages
 wget  -qO- https://github.com/xiaorouji/openwrt-passwall/archive/main.zip > temp1.zip
-#wget  -qO- https://github.com/kenzok8/small/archive/master.zip > temp2.zip
+wget  -qO- https://github.com/kenzok8/small/archive/master.zip > temp2.zip
 #wget  -qO- https://github.com/kenzok8/openwrt-packages/archive/master.zip > temp3.zip
 
 unzip -q temp1.zip
-#unzip -q temp2.zip
+unzip -q temp2.zip
 #unzip -q temp3.zip
 cp -r openwrt-passwall-main/luci-app-passwall package/
 
-#cp -r openwrt-packages-master/luci-app-adguardhome package/
+cp -r openwrt-packages-master/luci-app-adguardhome package/
 #cp -r openwrt-packages-master/adguardhome package/
 #cp -r openwrt-packages-master/luci-app-alist package/
 #cp -r openwrt-packages-master/alist package/
@@ -35,8 +35,8 @@ cp -r openwrt-passwall-main/luci-app-passwall package/
 #cp -r small-master/xray-core package/
 #cp -r small-master/chinadns-ng package/
 #cp -r small-master/sing-box package/
-#cp -r small-master/luci-app-mosdns package/
-#cp -r small-master/v2dat package/
+cp -r small-master/luci-app-mosdns package/
+cp -r small-master/v2dat package/
 #cp -r small-master/mosdns package/
 
 
@@ -44,8 +44,8 @@ cp -r openwrt-passwall-main/luci-app-passwall package/
 rm -rf openwrt-passwall-main
 rm -rf temp1.zip
 
-#rm -rf temp2.zip
-#rm -rf small-master
+rm -rf temp2.zip
+rm -rf small-master
 
 #rm -rf temp3.zip
 #rm -rf openwrt-packages-master
