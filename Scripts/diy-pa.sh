317ed92e173b89c6b07263ece2d6b1b96ef46dd5
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-04-29 12:34:03 +0800

    Update diy-pa.sh

--- Scripts/diy-pa.sh
+++ Scripts/diy-pa.sh
@@ -1,5 +1,7 @@
 #下载使用sbwml版本alist
-wget  -qO- https://github.com/sbwml/luci-app-alist/archive/master.zip > alist.zip
+wget -qO- https://github.com/lmq8267/luci-app-alist/archive/master.zip > alist.zip
+
+#wget  -qO- https://github.com/sbwml/luci-app-alist/archive/master.zip > alist.zip
 unzip -q alist.zip
 cp -r luci-app-alist-master/luci-app-alist package/
 rm -rf alist.zip
