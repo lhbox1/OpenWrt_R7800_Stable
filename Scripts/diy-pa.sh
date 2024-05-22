e86fc12683670a52702d240afb228e1be4e7abd9
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-05-22 21:12:43 +0800

    Update diy-pa.sh

--- Scripts/diy-pa.sh
+++ Scripts/diy-pa.sh
@@ -15,3 +15,24 @@ cp -r openwrt-passwall-4.77-5/luci-app-passwall package/
 rm -rf temp1.zip
 rm -rf openwrt-passwall-4.77-5
 
+#下载使用helloworld依赖库ssr
+wget -qO- https://github.com/fw876/helloworld/archive/master.zip > temp2.zip
+unzip -q temp2.zip
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+cp -r helloworld-master/luci-app-ssr-plus package/
+cp -r helloworld-master/shadow-tls package/
+rm -rf temp2.zip
+rm -rf helloworld-master
+
+#下载使用sbwml版本mosdns
+wget  -qO- https://github.com/sbwml/luci-app-mosdns/archive/v5.zip > mosdns.zip
+unzip -q mosdns.zip
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+cp -r luci-app-mosdns-5/luci-app-mosdns package/
+cp -r luci-app-mosdns-5/v2dat package/
+cp -r luci-app-mosdns-5/mosdns package/
+rm -rf mosdns.zip
+rm -rf luci-app-mosdns-5
+
