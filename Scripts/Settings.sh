f4d22f4ea82eab3c489a2a891d6939a747d92c42
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-06-25 01:44:52 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -33,6 +33,6 @@ git clone https://github.com/lhbox1/pakg.git package/pakggg
 
 git clone https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
 
-
+git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
 
 #git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
