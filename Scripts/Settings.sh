8abcca61eca8fa9fdd2bb6d5692648539718a734
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-09-29 21:52:16 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -3,6 +3,7 @@
 #删除冲突插件
 rm -rf $(find ./feeds/luci/ -type d -regex ".*\(openclash\|passwall\|ssr-plus\|alist\|easymesh\).*")
 rm -rf package/istore
+rm -rf feeds/packages/net/chinadns-ng
 ####
 #修改默认IP地址
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$OWRT_IP/g" ./package/base-files/files/bin/config_generate
@@ -14,3 +15,4 @@ sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" 
 #添加插件
 git clone https://github.com/lhbox1/luci-app-adguardhome1 package/luci-app-adguardhome
 git clone https://github.com/shuishihan/luci-app-easymesh.git package/luci-app-easymesh
+git clone https://github.com/lhbox1/pakg.git package/pakggg
