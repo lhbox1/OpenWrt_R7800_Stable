6e93943b9f67fe1378c58fbb13e20101fe7ec01e
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-10-01 08:29:30 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#增加主题
-#echo "CONFIG_PACKAGE_luci-theme-$OWRT_THEME=y" >> .config
-#echo "CONFIG_PACKAGE_luci-app-$OWRT_THEME-config=y" >> .config
+##-----------------Add AdGuardHome core------------------
+curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/lhbox1/Actions-OpenWrt/main/CMCC-RAX3000M-EMMC/AdGuardHome.tar.gz -o /tmp/AdGuardHome.tar.gz
+tar zxvf /tmp/AdGuardHome.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/AdGuardHome >/dev/null 2>&1
+mkdir -p package/luci-app-adguardhome/root/etc/AdGuardHome
+mv /tmp/AdGuardHome package/luci-app-adguardhome/root/etc/AdGuardHome/AdGuardHome >/dev/null 2>&1
+rm -rf /tmp/AdGuardHome.tar.gz >/dev/null 2>&1
 
-#根据源码来修改
-#if [[ $OWRT_URL != *"lede"* ]] ; then
-  #增加luci界面
-  #echo "CONFIG_PACKAGE_luci=y" >> .config
-  #echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
-#fi
 chmod +x files/etc/wan_check.sh
