ffb762b629c4d370f0218bd44f8ff3715d34437c
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-10-02 18:11:29 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -9,4 +9,4 @@ mv /tmp/AdGuardHome package/luci-app-adguardhome/root/etc/AdGuardHome/AdGuardHom
 rm -rf /tmp/AdGuardHome.tar.gz >/dev/null 2>&1
 
 chmod +x files/etc/wan_check.sh
-chmod +x files/etc/install_argon.sh
+chmod +x files/etc/install_ipk.sh
