5a73670e9ee84e554726914ea5320c960b261b12
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-10-02 17:19:55 +0800

    Update install_argon.sh

--- Config/files/etc/install_argon.sh
+++ Config/files/etc/install_argon.sh
@@ -3,7 +3,7 @@
 
 opkg install /etc/argon/*.ipk
 
-sed -i "s/7e924a76db88dbc0724c8676d5a1a221/71d39ab23054c352744e74fe3a674e50/g" /usr/lib/opkg/status
+sed -i "s/kernel (.*)/kernel (= 5.4.255-1-71d39ab23054c352744e74fe3a674e50)/g" /usr/lib/opkg/status
 sleep 1
 sed -i "s/install_argon.sh/wan_check.sh/g" /etc/crontabs/root
 
