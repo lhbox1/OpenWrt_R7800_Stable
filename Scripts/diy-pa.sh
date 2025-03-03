676ef29202f361c079dd24086810471b646a03a9
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-03-04 05:07:14 +0800

    Update diy-pa.sh

--- Scripts/diy-pa.sh
+++ Scripts/diy-pa.sh
@@ -1,29 +1,29 @@
 #下载使用lucky
-#wget  -qO- https://codeload.github.com/gdy666/luci-app-lucky/zip/refs/tags/v2.10.5 > lucky.zip
-#unzip -q lucky.zip
-#cp -r luci-app-lucky-2.10.5/luci-app-lucky package/
-#cp -r luci-app-lucky-2.10.5/lucky package/
-#rm -rf lucky.zip
-#rm -rf luci-app-lucky-2.10.5
+wget  -qO- https://codeload.github.com/gdy666/luci-app-lucky/zip/refs/tags/v2.15.7 > lucky.zip
+unzip -q lucky.zip
+cp -r luci-app-lucky-2.15.7/luci-app-lucky package/
+cp -r luci-app-lucky-2.15.7/lucky package/
+rm -rf lucky.zip
+rm -rf luci-app-lucky-2.15.7
 
 
 #下载使用master版本passwall
 #wget -qO- https://github.com/xiaorouji/openwrt-passwall/archive/main.zip > temp1.zip
-wget -qO- https://codeload.github.com/xiaorouji/openwrt-passwall/zip/refs/tags/24.12.08-1 > temp1.zip
-unzip -q temp1.zip
-rm -rf feeds/luci/applications/luci-app-passwall
-cp -r openwrt-passwall-24.12.08-1/luci-app-passwall package/
-rm -rf temp1.zip
-rm -rf openwrt-passwall-24.12.08-1
+#wget -qO- https://codeload.github.com/xiaorouji/openwrt-passwall/zip/refs/tags/24.12.08-1 > temp1.zip
+#unzip -q temp1.zip
+#rm -rf feeds/luci/applications/luci-app-passwall
+#cp -r openwrt-passwall-24.12.08-1/luci-app-passwall package/
+#rm -rf temp1.zip
+#rm -rf openwrt-passwall-24.12.08-1
 
 #下载使用helloworld依赖库ssr
-wget -qO- https://github.com/fw876/helloworld/archive/master.zip > temp2.zip
-unzip -q temp2.zip
-rm -rf feeds/luci/applications/luci-app-ssr-plus
-cp -r helloworld-master/luci-app-ssr-plus package/
-cp -r helloworld-master/shadow-tls package/
-rm -rf temp2.zip
-rm -rf helloworld-master
+#wget -qO- https://github.com/fw876/helloworld/archive/master.zip > temp2.zip
+#unzip -q temp2.zip
+#rm -rf feeds/luci/applications/luci-app-ssr-plus
+#cp -r helloworld-master/luci-app-ssr-plus package/
+#cp -r helloworld-master/shadow-tls package/
+#rm -rf temp2.zip
+#rm -rf helloworld-master
 
 #下载使用sbwml版本mosdns
 #wget  -qO- https://codeload.github.com/sbwml/luci-app-mosdns/zip/refs/tags/v5.3.1 > mosdns.zip
