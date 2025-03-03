9ab5d3bf7ebc56dbeb8e5d0feeca59bb14339084
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-03-04 05:18:05 +0800

    Update install_ipk.sh

--- Config/files/etc/install_ipk.sh
+++ Config/files/etc/install_ipk.sh
@@ -6,6 +6,7 @@ opkg install /etc/ipk/*.ipk --force-downgrade
 sed -i "s/kernel (.*)/kernel (= 5.4.255-1-71d39ab23054c352744e74fe3a674e50)/g" /usr/lib/opkg/status
 sleep 1
 sed -i "s/install_ipk.sh/wan_check.sh/g" /etc/crontabs/root
-
+#重启定时命令
+/etc/init.d/cron restart
 rm -rf /etc/ipk
 rm -rf /etc/install_ipk.sh
