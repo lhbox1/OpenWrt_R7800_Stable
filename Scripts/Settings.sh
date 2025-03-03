5ee102cbb44c69724d3c53967368af4ed84ea65a
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-03-04 04:43:29 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -24,5 +24,5 @@ sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" 
 #添加插件
 git clone https://github.com/lhbox1/luci-app-adguardhome1 package/luci-app-adguardhome
 git clone https://github.com/shuishihan/luci-app-easymesh.git package/luci-app-easymesh
-git clone https://github.com/lhbox1/pakg.git package/pakggg
+# git clone https://github.com/lhbox1/pakg.git package/pakggg
 git clone --depth=1 --single-branch --branch "main" https://github.com/lmq8267/luci-app-alist ./package/luci-app-alist
