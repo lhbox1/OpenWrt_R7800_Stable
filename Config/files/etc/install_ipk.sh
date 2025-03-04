a039a5e183676d534f17e89f1ea36955b2539a29
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-03-04 20:45:50 +0800

    Update install_ipk.sh

--- Config/files/etc/install_ipk.sh
+++ Config/files/etc/install_ipk.sh
@@ -4,6 +4,7 @@
 opkg install /etc/ipk/*.ipk --force-downgrade
 
 sed -i "s/kernel (.*)/kernel (= 5.4.255-1-71d39ab23054c352744e74fe3a674e50)/g" /usr/lib/opkg/status
+sed -i "s/mirrors.*openwrt/downloads.immortalwrt.org/g" /etc/opkg/distfeeds.conf
 sleep 1
 sed -i "s/install_ipk.sh/wan_check.sh/g" /etc/crontabs/root
 #重启定时命令
