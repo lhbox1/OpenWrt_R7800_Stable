3a28aab7884eb511b5f72b60b99ac4fbf7719e7b
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-03-13 10:54:23 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ##-----------------Add AdGuardHome core------------------
-# curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/lhbox1/Actions-OpenWrt/main/CMCC-RAX3000M-EMMC/AdGuardHome.tar.gz -o /tmp/AdGuardHome.tar.gz
-# tar zxvf /tmp/AdGuardHome.tar.gz -C /tmp >/dev/null 2>&1
-# chmod +x /tmp/AdGuardHome >/dev/null 2>&1
-# mkdir -p package/luci-app-adguardhome/root/etc/AdGuardHome
-# mv /tmp/AdGuardHome package/luci-app-adguardhome/root/etc/AdGuardHome/AdGuardHome >/dev/null 2>&1
-# rm -rf /tmp/AdGuardHome.tar.gz >/dev/null 2>&1
+curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/lhbox1/Actions-OpenWrt/main/CMCC-RAX3000M-EMMC/AdGuardHome.tar.gz -o /tmp/AdGuardHome.tar.gz
+tar zxvf /tmp/AdGuardHome.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/AdGuardHome >/dev/null 2>&1
+mkdir -p package/luci-app-adguardhome/root/etc/AdGuardHome
+mv /tmp/AdGuardHome package/luci-app-adguardhome/root/etc/AdGuardHome/AdGuardHome >/dev/null 2>&1
+rm -rf /tmp/AdGuardHome.tar.gz >/dev/null 2>&1
 
 chmod +x files/etc/wan_check.sh
 chmod +x files/etc/install_ipk.sh
