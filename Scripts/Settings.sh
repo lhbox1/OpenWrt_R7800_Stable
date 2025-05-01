1ff0b2ade12b16c9eb711a6f3a62c2ab45df38b9
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-05-01 17:56:31 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #删除冲突插件
-rm -rf $(find ./feeds/luci/ -type d -regex ".*\(openclash\|passwall\|ssr-plus\|lucky\|alist\|easymesh\|adguardhome\|ksmbd\).*")
+rm -rf $(find ./feeds/luci/ -type d -regex ".*\(openclash\|passwall\|smartdns\|lucky\|alist\|easymesh\|adguardhome\|ksmbd\).*")
 rm -rf package/istore
 
 #passwall
@@ -46,7 +46,7 @@ git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/l
 
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 
-
+git clone -b master https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
 git clone https://github.com/lhbox1/luci-app-adguardhome1 package/luci-app-adguardhome
 git clone https://github.com/shuishihan/luci-app-easymesh.git package/luci-app-easymesh
