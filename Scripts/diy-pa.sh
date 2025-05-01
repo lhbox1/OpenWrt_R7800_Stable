0e955863568184a83f8f645cad965745a0ac69aa
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-05-02 06:41:31 +0800

    Update diy-pa.sh

--- Scripts/diy-pa.sh
+++ Scripts/diy-pa.sh
@@ -16,25 +16,11 @@ cp -r openwrt-passwall-25.5.1-2/luci-app-passwall package/
 rm -rf temp1.zip
 rm -rf openwrt-passwall-25.5.1-2
 
-#下载使用helloworld依赖库ssr
-#wget -qO- https://github.com/fw876/helloworld/archive/master.zip > temp2.zip
-#unzip -q temp2.zip
-#rm -rf feeds/luci/applications/luci-app-ssr-plus
-#cp -r helloworld-master/luci-app-ssr-plus package/
-#cp -r helloworld-master/shadow-tls package/
-#rm -rf temp2.zip
-#rm -rf helloworld-master
 
-#下载使用sbwml版本mosdns
-#wget  -qO- https://codeload.github.com/sbwml/luci-app-mosdns/zip/refs/tags/v5.3.1 > mosdns.zip
-#unzip -q mosdns.zip
-#rm -rf feeds/packages/net/mosdns
-#rm -rf feeds/packages/net/v2ray-geodata
-#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-#cp -r luci-app-mosdns-5.3.1/luci-app-mosdns package/
-#cp -r luci-app-mosdns-5.3.1/v2dat package/
-#cp -r luci-app-mosdns-5.3.1/mosdns package/
-#rm -rf mosdns.zip
-#rm -rf luci-app-mosdns-5.3.1
+git clone --depth=1 --single-branch --branch "master" https://github.com/kenzok8/openwrt-packages.git ./small-openwrt-packages
+cp -r small-openwrt-packages/luci-app-smartdns package/
+cp -r small-openwrt-packages/smartdns package/
+
+rm -rf small-openwrt-packages
 
