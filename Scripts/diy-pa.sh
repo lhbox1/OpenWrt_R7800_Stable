51f4dbb2d516c42f211ad6bb934872f989220e8b
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-05-01 18:07:37 +0800

    Update diy-pa.sh

--- Scripts/diy-pa.sh
+++ Scripts/diy-pa.sh
@@ -8,6 +8,11 @@ rm -rf luci-app-lucky-2.15.7
 
 
 #下载使用master版本passwall
+
+
+rm -rf /feeds/packages/net/smartdns/Makefile
+cp -r Makefile /feeds/packages/net/smartdns/
+
 #wget -qO- https://github.com/xiaorouji/openwrt-passwall/archive/main.zip > temp1.zip
 wget -qO- https://codeload.github.com/xiaorouji/openwrt-passwall/zip/refs/tags/25.4.20-1 > temp1.zip
 unzip -q temp1.zip
