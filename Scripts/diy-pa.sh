e4f7d74e89ddbb563581be480d7f1673a3fdd28b
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-05-16 21:08:07 +0800

    Update diy-pa.sh

--- Scripts/diy-pa.sh
+++ Scripts/diy-pa.sh
@@ -8,17 +8,17 @@ rm -rf luci-app-lucky-2.15.7
 
 
 #下载使用master版本passwall
-#wget -qO- https://github.com/xiaorouji/openwrt-passwall/archive/main.zip > temp1.zip
-wget -qO- https://codeload.github.com/xiaorouji/openwrt-passwall/zip/refs/tags/25.5.16-1 > temp1.zip
+wget -qO- https://github.com/xiaorouji/openwrt-passwall/archive/main.zip > temp1.zip
+#wget -qO- https://codeload.github.com/xiaorouji/openwrt-passwall/zip/refs/tags/25.5.16-1 > temp1.zip
 
 unzip -q temp1.zip
 
-#cp -r openwrt-passwall-main/luci-app-passwall package/
-cp -r openwrt-passwall-25.5.16-1/luci-app-passwall package/
+cp -r openwrt-passwall-main/luci-app-passwall package/
+#cp -r openwrt-passwall-25.5.16-1/luci-app-passwall package/
 rm -rf temp1.zip
 
-#rm -rf openwrt-passwall-main
-rm -rf openwrt-passwall-25.5.16-1
+rm -rf openwrt-passwall-main
+#rm -rf openwrt-passwall-25.5.16-1
 
 git clone --depth=1 --single-branch --branch "master" https://github.com/kenzok8/openwrt-packages.git ./small-openwrt-packages
 cp -r small-openwrt-packages/luci-app-smartdns package/
